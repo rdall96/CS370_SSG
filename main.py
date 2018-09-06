8460a6e75d3cceb3e77f189a2d9493641b3e6909
Ricky Dall'Armellina <rdall96@gmail.com>
2018-09-06 13:03:22 -0400

    Created main
    currently empty, but ready for code to be input

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+#----------------------------------------------------------------------
+# Static Server Generator
+# Author: CS 370 group
+# Date: 09/6/2018
+#
+# Description: This application takes a folder of markdown files and
+#              translates them to a working HTML website, creating a
+#              separate folder with the correct hierarchy.
+#----------------------------------------------------------------------
