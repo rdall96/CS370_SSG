486e40d093176bb365b3955e3e9401d4d50abc75
lukeylad19 <37940137+lukeylad19@users.noreply.github.com>
2018-09-06 15:28:05 -0400

    Updated directoryCopyTest.py with dirCopy():

--- Tests/Directory Copy/directoryCopyTest.py
+++ Tests/Directory Copy/directoryCopyTest.py
@@ -1,12 +1,17 @@
-import os
 
-inputpath = 'Markdown/'
-outputpath = 'HTML'
+def dirCopy():
 
-for dirpath, dirnames, filenames in os.walk(inputpath):
-    structure = os.path.join(outputpath, dirpath[len(inputpath):])
-    if not os.path.isdir(structure):
-        os.mkdir(structure)
-    else:
-        print("Folder already exists")
-        break 
+    import os
+
+    inputpath = 'Markdown/'
+    outputpath = 'HTML'
+
+    for dirpath, dirnames, filenames in os.walk(inputpath):
+        structure = os.path.join(outputpath, dirpath[len(inputpath):])
+        if not os.path.isdir(structure):
+            os.mkdir(structure)
+        else:
+            print("Folder already exists")
+            break
+
+dirCopy()
