507912fd2329b38513c91c9ebc33dc722ffc441f
Ricky Dall'Armellina <rdall96@gmail.com>
2018-09-10 20:52:46 -0400

    Added theme selction to main

--- main.py
+++ main.py
@@ -10,26 +10,48 @@
 #              separate folder with the correct hierarchy.
 #----------------------------------------------------------------------
 
+DEBUG = True
+
 # Import statements
 import src.markdown2html as Converter
 
 # Other functions
+
+def LOG(string):
+    if DEBUG:
+        print(string)
 
 #----------------------------------------------------------------------
 #----------------------------------------------------------------------
 print(" - Welcome to the Static Site Generator! - ")
 
 # Ask user for folder path with markdown files
-markdownFolder = raw_input("Insert path to markdown documents: ")
+LOG("Insert path to markdown documents: ")
+markdownFolder = raw_input()
 
-# Analyze and recurse through folder
+# Ask user to pick a theme for the website
+LOG("Choose a theme: \n 1. Light theme\n 2. Dark theme\n 3. Fun theme")
+themeOption = raw_input()
+if themeOption == '1':
+    #light theme selected
+    LOG("Light theme choosen")
+    userTheme = "light_theme"
+elif themeOption == '2':
+    #dark theme selected
+    LOG("Dark theme choosen")
+    userTheme = "dark_theme"
+elif themeOption == '3':
+    #dark theme selected
+    LOG("Fun theme choosen")
+    userTheme = "fun_theme"
+else:
+    LOG("The selcted theme does not exist")
+
+# Analyze folder and build stub dictonary
 
 
-# Create hierarchy list
+# Create 'HTML' folder with same hierarchy as 'Markdown' folder
 
 
-# Convert files to HTML
-
-
-# Rebuild hierarchy and folder for HTML site
-
+# Copy files to 'HTML' folder
+# If the file's extension is '.md' then convert it
