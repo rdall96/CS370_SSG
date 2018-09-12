b354da5b566dc76aeb4183ec33ece8af2d7da770
lukeylad19 <37940137+lukeylad19@users.noreply.github.com>
2018-09-12 14:39:52 -0400

    Markdown Checker and file copy

---
+++ Tests/FIle copy and MD Checker/fileCopy.py
@@ -0,0 +1,18 @@
+
+
+import os
+import shutil
+import checkMD.py as Checker
+
+
+def fileCopy(srcDir, destDir):
+
+    src_files = os.listdir(srcDir)
+    for fileName in src_files:
+        fullFileName = os.path.join(src, fileName)
+        if (os.path.isfile(fileName)):
+            Checker.checkMDfile(fileName)
+            shutil.copy(fullFileName, destDir)
+
+#-----------------------------------------------------------------------------
+fileCopy('Markdown' , 'HTML')
