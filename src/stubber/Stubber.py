ad37b3a65176c7e1473d796ece52709ccbc66973
Ricky Dall'Armellina <rdall96@gmail.com>
2018-09-12 15:11:49 -0400

    Moved Stubber to src
    Moved Stubber to its own src folder.
    Added static definition for dictionary.txt file

---
+++ src/stubber/Stubber.py
@@ -0,0 +1,58 @@
+#Stub generator and dictionary
+#READ ME
+#Pass stubGen a file path to generate a stub, write it to the file, and add it to the dict
+#Pass stubCheck a file path and stub to check if they match, if not, delete that line from dict
+#and add the new file path and stub to the dict
+#The dictionary will be written to Dictionary_output.txt
+#Derek Connelly 9/4/18
+
+
+import random
+import string
+import collections
+import json
+
+fileDict = {}
+STUB_DICTONARY = "src/stubber/Dictionary_output.txt"
+
+
+def stubGen(pathTemp):
+    if pathTemp not in fileDict.keys():
+        stubTemp = ""
+        for x in range(4):
+            temp = random.randint(0, 1)
+            if temp == 1:
+                stubTemp = stubTemp + str(random.randint(0, 9))
+            else:
+                stubTemp = stubTemp + random.choice(string.ascii_letters)
+        fileDict[pathTemp] = stubTemp
+        fileDictJson= json.dumps(collections.OrderedDict(sorted(fileDict.items())))
+        output = open(STUB_DICTONARY, "w")
+        output.write(fileDictJson)
+        output.close()
+        line = '<!-- ' + stubTemp + '-->\n'
+        f = open(pathTemp, "r+")
+        file_data = f.read()
+        f.seek(0,0)
+        f.write(line.rstrip('\r\n') + '\n' + file_data)
+        f.close()
+
+
+def stubCheck(pathTemp, stubTemp):
+        if stubTemp in fileDict.values() and (list(fileDict.keys())[list(fileDict.values()).index(stubTemp)] != pathTemp):
+            del fileDict[list(fileDict.keys())[list(fileDict.values()).index(stubTemp)]]
+            fileDict[pathTemp] = stubTemp
+            fileDictJson = json.dumps(collections.OrderedDict(sorted(fileDict.items())))
+            output = open(STUB_DICTONARY, "w")
+            output.write(fileDictJson)
+            output.close()
+
+
+def loadDict(nfile):
+    with open(nfile, 'r') as inf:
+        fileDict = eval(inf.read())
+    fileDictJson = json.dumps(collections.OrderedDict(sorted(fileDict.items())))
+    output = open(STUB_DICTONARY, "w")
+    output.write(fileDictJson)
+    output.close()
+
