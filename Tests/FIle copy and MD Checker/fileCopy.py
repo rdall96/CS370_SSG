f1de1302bb638414c0466c5823a8def6882891df
lukeylad19 <37940137+lukeylad19@users.noreply.github.com>
2018-09-12 14:54:57 -0400

    Fixed variable erro

--- Tests/FIle copy and MD Checker/fileCopy.py
+++ Tests/FIle copy and MD Checker/fileCopy.py
@@ -9,7 +9,7 @@ def fileCopy(srcDir, destDir):
 
     src_files = os.listdir(srcDir)
     for fileName in src_files:
-        fullFileName = os.path.join(src, fileName)
+        fullFileName = os.path.join(srcDir, fileName)
         if (os.path.isfile(fileName)):
             Checker.checkMDfile(fileName)
             shutil.copy(fullFileName, destDir)
