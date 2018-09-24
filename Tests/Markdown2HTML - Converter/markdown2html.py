34a788e511dec04efcc6ddb5d302e6ca68fbcdb0
Ricky Dall'Armellina <rdall96@gmail.com>
2018-09-24 14:50:37 -0400

    Re-created Markdown to HTML Converter
    Re-wrote converter, polished it a bit more and added check for real markdown file.

--- Tests/Markdown2HTML - Converter/markdown2html.py
+++ Tests/Markdown2HTML - Converter/markdown2html.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
-
 #----------------------------------------------------------------------
 # Markdown to HTML Translator
 # Author: Ricky Dall'Armellina
-# Date: 09/3/2018
+# Date: 09/24/2018
 #
 # Description: Translates a text file written in Markdown to a working
-# HTML document and saves it with the same name
+#              HTML document and saves it with the same name.
+#              Takes the full path of a markdown file and creates an
+#              HTML file in the same location.
+#              Returns true when successful and false when not.
 #----------------------------------------------------------------------
 DEBUG = False
 
@@ -14,67 +16,78 @@ import os
 import markdown
 import markdown_extensions
 
+HTML_HeaderFile = "html_head.txt"
+EXECUTE_SUCCESSFULLY = False
+
 def LOG(string):
     if DEBUG:
         print(string)
 
 def getFileName(fileName):
-    #get a substring with just the file name, no extension
-    name = fileName[:-3]
-    #print(name)
+    # get a substring with just the file name, no extension
+    name = fileName[:-3] # extension = '.md' 3 chars
+    LOG("File name: " + name)
     return name
 
-def checkExistingFile(fileName):
-    htmlFileName = getFileName(fileName) + ".html"
-    #check if file exits and delete it
-    fileExists = os.path.isfile(htmlFileName)
-    if fileExists:
-        LOG("Removing previously created html file")
-        os.remove(htmlFileName)
-    else:
-        LOG("HTML file doesn't exist, continuing...")
-
 def generateHTMLHeader(file, mode):
-    #generates html opening and closing elements
-    #take the html file to ad headers to and a mode as an argument
-    #mode 'o' (opening statements)
-    #mode 'c' (closing statements)
+    # generates html opening and closing elements
+    # take the html file to ad headers to and a mode as an argument
+    # mode 'o' (opening statements)
+    # mode 'c' (closing statements)
     if mode == 'o':
-        headFile = open("html_head.txt", "r", 0)
+        headFile = open(HTML_HeaderFile, "r", 0)
         file.write(headFile.read())
+        # close the header file
         headFile.close()
     elif mode == 'c':
         file.write("\n</body>")
+        # close the html file, done writing to it
+        file.close()
     return file
 
 def createHTML(fileName):
-    #get name from getFileName() and create html file with that name
+    # get name from getFileName() and create html file with that name
     LOG("Creating HTML file...")
     htmlName = getFileName(fileName) + ".html"
+    # Creates html file in the same location as the markdown file
     html = open(htmlName, "w+", 0)
+    # adds HTML header from html_head file
     html = generateHTMLHeader(html, 'o')
+    # return HTML file object
     return html
 
 def parseMarkdown(mFile, htmlFile):
-    #call Markdown library to open the textFile and read it
+    # try catch in case the file can't open, can't be read or can't be converted
     try:
+        # open the textFile and read it
         input_file = open(mFile, "r", 0)
-    except OSError as e:
+        # read inputFile as a string
+        text = input_file.read()
+        # converter returns an HTML string of text
+        html = markdown.markdown(text, ['markdown_extensions'])
+        # write the converted html text to the html file
+        htmlFile.write(html)
+        EXECUTE_SUCCESSFULLY = True
+    except OSError as err:
         LOG("Cannot open the target file: " + mFile)
         LOG("Perhaps the path to the file is incorrect?")
-        LOG(e)
-    text = input_file.read()
-    #input the contents of the markdown file into the html file
-    html = markdown.markdown(text, ['markdown_extensions'])
-    #write the converted html text to the html file
-    htmlFile.write(html)
+        EXECUTE_SUCCESSFULLY = False
+        LOG(err)
+    except ValueError:
+        LOG("Couldn't read the contents of the file")
+        EXECUTE_SUCCESSFULLY = False
+    except:
+        LOG("Unexpected error, could not convert the file")
+        EXECUTE_SUCCESSFULLY = False
     return htmlFile
 
 def markdown2html(inputFile):
     LOG("- Markdown to HTML Python Translator -\n")
-    #create html file
+    # create html file
     htmlFile = createHTML(inputFile)
-    #parse and analyze the markdown file and obtain the html file
+    # parse and analyze the markdown file and obtain the html file
     outputFile = parseMarkdown(inputFile, htmlFile)
+    # add closing headers for HTML
     generateHTMLHeader(outputFile, "c")
-    return outputFile
+    # return if execute successfully or not
+    return EXECUTE_SUCCESSFULLY
--- Tests/Markdown2HTML - Converter/old/class_test.py
+++
@@ -1,5 +0,0 @@
-import markdown2html as Converter
-
-input = raw_input("select file: ")
-output = Converter.markdown2html(input)
-#print(output)
