5802705dd8b71d5499675e3b8c4f7e323bfdada2
Reinok <42870971+Reinok@users.noreply.github.com>
2018-09-27 15:05:17 -0400

    asset monitor updates

--- Tests/AssetManagement/assetMonitor.py
+++ Tests/AssetManagement/assetMonitor.py
@@ -10,18 +10,23 @@
 import os
 import dirNaviV1 as Navi
 import Stubber as dictionary
+import string
 #checks if any assets in the MD file structure have moved
-def checkAssetStructure(searchFolder):
-    for each line in dictionary.getStub():
-        realPath = Navi.findTargetFile(assetStub, searchFolder)
-        if(not(realPath == dictionary.getPath(assetStub))):
-            dictionary.setPath(assetStub, realPath)
+#def checkAssetStructure(searchFolder):
+#    for each line in dictionary.getStub():
+#        realPath = Navi.findTargetFile(assetStub, searchFolder)
+#        if(not(realPath == dictionary.getPath(assetStub))):
+#            dictionary.setPath(assetStub, realPath)
 
 def convertStubsToLinks(searchFolder):
     for root, dirs, files in os.walk(searchFolder):
         if("\.md" in files):
-            for each line in files:
-                if(line.find("\[.*\]\(.*\)$")):
-                    tempStub = line
-                    string.replace(tempStub, "^.*\[.*\]\((.*)\)$", "\1")
+            with open(files, "r") as ins:
+                for line in ins:
+                    if(line.find("\[.*\]\(.*\)$")):
+                        print("Found link\n")
+                        tempStub = line
+                        tempStub.replace( "^.*\[.*\]\((.*)\)$", "\1")
+                        linkPath = dictionary.getPath(tempStub)
+                        line.replace(tempStub, linkPath)
     return -1
