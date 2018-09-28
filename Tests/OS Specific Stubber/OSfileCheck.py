2cbc25d573a4f2efa4b4e67bdd54b5de79305cbc
Ricky Dall'Armellina <rdall96@gmail.com>
2018-09-28 13:54:02 -0400

    Update OSfileCheck with some code revisions
    Cose would break at line 42 when trying to analyze a NULL path

--- Tests/OS Specific Stubber/OSfileCheck.py
+++ Tests/OS Specific Stubber/OSfileCheck.py
@@ -3,13 +3,12 @@
 # Author: Lukas Mallory
 # Date: 09/25/2018
 #
-# Description: Determines the user's operating system and checks to see
+# Description: Determines the user's operating system and checks to see 
 # if a given file exists.
 #---------------------------------------------------------------------
 
 import platform
 import os
-import getpass
 import Stubber as Stub
 
 
@@ -20,29 +19,47 @@ def findFile(name, path):
 
 def systemCheck():
     usrFileInput = raw_input("Enter the file name: ")
+    # creating empty PathInput
+    PathInput = ""
     usrPlatform = platform.system()
-    usrName = getpass.getuser()
-#    print usrName
     if usrPlatform == "Linux":
-        PathInput = "/home/" + usrName
+        PathInput = "/home"
         fullFilePath = findFile(usrFileInput , PathInput)
 #        print fullFilePath
-
     elif usrPlatform == "Windows":
-        PathInput = "C:\\" + "Users\\" + usrName
+        PathInput = "C:" + "\\"
         fullFilePath = findFile(usrFileInput , PathInput)
 #        print fullFilePath
-
     elif usrPlatform == "Darwin":
-        PathInput = "/Users/" + usrName
+        PathInput = "/Users/"
         fullFilePath = findFile(usrFileInput , PathInput)
 #        print fullFilePath
-    if os.path.isfile(fullFilePath):
-        Stub.createDict(fullFilePath)
+    # added else check for not supported OSes
+    # Ricky Dall'Armellina - 09/28/2018
     else:
-        print("ERROR!")
+        print("OS not supported")
+
+    # FIXED: code would break when trying to check fo .isfile with NULL fullFilePath
+    # Author: Ricky Dall'Armellina
+    # Date: 09/28/2018
+    # Wrapped check in try catch to prevent breaking
+    try:
+        if os.path.isfile(fullFilePath):
+            Stub.createDict(fullFilePath)
+        else:
+            print("ERROR! Could not find file: " + usrFileInput)
+    except TypeError as typeErr:
+        print("File  " + usrFileInput + "  does not exist under any file path:  " + PathInput)
+        #print(typeErr)
+        print("If your directory is stored in an external drive, please move it under your OS drive")
+    except ValueError as valErr:
+        print("File  " + usrFileInput + "  does not exist under any file path:  " + PathInput)
+        #print(valErr)
+        print("If your directory is stored in an external drive, please move it under your OS drive")
+    except:
+        print("Unexpected error!")
+    return
+
+#-----------------------------------------------------------------------------------
+
 systemCheck()
-
-
-
-#make a program to auto-rename files and put stubs in them
