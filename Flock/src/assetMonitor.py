dc3f93848d0ff19c876031567bf6c657ed0df218
Ricky Dall'Armellina <rdall96@gmail.com>
2018-10-08 15:29:49 -0400

    Added Asset Monitor to Flock main
    Changed imports and hard-coded paths to work with main.py

--- Flock/src/assetMonitor.py
+++ Flock/src/assetMonitor.py
@@ -8,25 +8,69 @@
 #    Langauge: Python                           #
 #################################################
 
-import os
 
-import navigator.dirNaviV1 as Navi
-import stubber.Stubber as dictionary
+from tempfile import mkstemp
+from shutil import move
+from os import fdopen, remove
+import navigator.dirNaviV2 as Navi
+import stubber.Stubber2 as dictionary
 import string
-import platform
+import re
+import os
+#checks if any assets in the MD file structure have moved
+def checkAssetStructure(searchFolder):
+    stubList = dictionary.getStubList()
+    for stub in range(len(stubList)):
+        realPath = Navi.findTargetFile(stub, searchFolder)
+        if(not(realPath == dictionary.getPath(stub))):
+            dictionary.changePath(stub, realPath)
 
 def convertStubsToLinks(searchFolder):
+    stubList = dictionary.getStubList()
     for root, dirs, files in os.walk(searchFolder):
-        if("\.md" in files):
-            topDir = platform.system
-            mdFilePath = Navi.findTargetFile(files, topDir)
-            with open(files, "r") as ins:
-                for line in ins:
-                    if(line.find("\[.*\]\(.*\)$")):
-                        print("Found link\n")
-                        tempStub = line
-                        tempStub.replace( "^.*\[.*\]\((.*)\)$", "\1")
-                        linkPath = dictionary.getPath(tempStub)
-                        line.replace(tempStub, linkPath)
-    return -1
+        for filename in files:
+            if(".md" in filename):
+                topDir = searchFolder
+                mdFilePath = os.path.join(root, filename)
+                #Below opens Md file to parse lines and creates temp file to store new lines
+                fh, absPath = mkstemp()
+                with fdopen(fh,'w') as new_file:
+                    with open(mdFilePath, "r+") as old_file:
+                        for line in old_file:
+                            #If statement below checks if the link syntax is in a line
+                            if(re.search("\[.*\]\(.*\)", line, flags = 0)):
+                                print("Found link\n")
+                                tempLine = line
+                                tempLine = re.sub("^.*\[.*\]", "", tempLine)
+                                parsedLine = re.split("[()]", tempLine)
+                                tempStub = parsedLine[1]
+                                stubInDict = False
+                                for stub in stubList:
+                                    if(tempStub == stub):
+                                        stubInDict = True
+                                if(stubInDict == False):
+                                    print("Stub, ", tempStub, ", not in dictionary")
 
+                                #Code block below extracts stub, gets the corresponding path from the dictionary
+                                #and replaces the stub in the line with the path from the dictionary
+                                linkPath = dictionary.getPath(tempStub)
+                                if(linkPath == -1):
+                                    print("Stub, ", tempStub, ", corresponding path not found in dictionary")
+                                    print("Substitution will not be carried out, check stubs in files for errors.")
+                                    new_file.write(line)
+                                else:
+                                    #Getting path of the link and replacing all "\" with "\\" to prevent \n, \t, etc.
+                                    linkPath = re.sub(r"\\", r"\\\\", linkPath)
+                                    linkPath = "(" + linkPath + ")"
+                                    tempStub = "\(" + tempStub + "\)"
+                                    tempLine = line
+                                    tempLine = re.sub(tempStub, linkPath, tempLine)
+                                    new_file.write(tempLine)
+                                print(tempLine)
+                            else:
+                                new_file.write(line)
+                #Deleting old MD file and moving new file to replace it
+                remove(mdFilePath)
+                move(absPath, mdFilePath)
+
+    return -1
