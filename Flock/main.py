99f1b108c2e491a48001bd0e8067fd2e562cc356
Ricky Dall'Armellina <rdall96@gmail.com>
2018-10-25 12:10:16 -0400

    feature: renaming index html file

--- Flock/main.py
+++ Flock/main.py
@@ -9,7 +9,7 @@
 #              separate folder with the correct hierarchy.
 #----------------------------------------------------------------------
 
-DEBUG = True
+DEBUG = False
 
 # Import statements
 import os, shutil, sys
@@ -19,6 +19,7 @@ import src.stubber.Stubber2 as Stubber
 import src.copier.fileCopy as Copier
 import src.OScheckLooper as OSutil
 import src.assetMonitor as Asset
+import src.argChecker as ArgCheck
 
 # Other functions
 
@@ -29,6 +30,14 @@ def LOG(string):
 
 def getFullPath(folder):
     return os.path.abspath(folder + '/')
+
+def renameIndex(folder):
+    # renames index website file - file name needs to contain index as stub
+    newName = folder + "/index.html"
+    currName = (folder + "/" + Stubber.getPath("index")) # get path to file with stub index
+    LOG("   Index file: " + currName)
+    os.rename(currName, newName)
+    return
 
 def selectTheme(destFolder):
     print("Available themes: \n 1. Light theme\n 2. Dark theme\n 3. Fun theme")
@@ -60,42 +69,48 @@ def selectTheme(destFolder):
 #----------------------------------------------------------------------
 
 print("\n - Welcome to the Static Site Generator! - \n")
-UsageDocs.showDocs(sys.argv)
+UsageDocs.showDocs(0)
+if ArgCheck.parse(sys.argv) == False:
+    DEBUG = True
 
 # Ask user for folder path with markdown files
 # call Lukes OS specific folder finder
 markdownFolder = OSutil.systemCheck(raw_input("Insert path to markdown documents: "))
-#LOG("Source folder:  " + markdownFolder)
+LOG("Source folder:  " + markdownFolder)
 # Adding '/www' to the destination path to make sure it's an non-existing path to save the website
 htmlFolder = OSutil.systemCheck(raw_input("Insert path to the website folder: ")) + "/www"
-#LOG("Destination folder:  " + htmlFolder)
+LOG("Destination folder:  " + htmlFolder)
 
 # Create htmlFolder directory structure
     # Copy all files to it
-#LOG("\ncopying files")
+LOG("\ncopying files")
 Copier.fileCopy(markdownFolder, htmlFolder)
 
 # Call stub dictonary generation on destination folder
     # OS specific
-#LOG("\ngenerating dictonary")
+LOG("\ngenerating dictonary")
 indexedFiles = Stubber.populateDict(htmlFolder)
 LOG("   Indexed " + (str)(indexedFiles) + " files")
 
 # Check if files are valid
     # Call Asset Monitor
-#LOG("\nmigrating links")
+LOG("\nmigrating links")
 Asset.convertStubsToLinks(htmlFolder)
 
 # Convert files
-#LOG("\nconverting files")
+LOG("\nconverting files")
 filesConverted = Converter.convertAllMarkdown(htmlFolder)
-#LOG("   Converted " + (str)(filesConverted) + " files")
+LOG("   Converted " + (str)(filesConverted) + " files")
 # Delete markdown files from destinantion
-#LOG("\ndeleting original .md files from destination")
+LOG("\ndeleting original .md files from destination")
 Copier.deleteMD(htmlFolder)
 
+# Rename main website (index) file to "index.html"
+LOG("\nrenaming index file")
+renameIndex(htmlFolder)
+
 # Ask user to pick a theme for the website and copy it to destination folder
-#LOG("\nselecting theme")
+LOG("\nselecting theme")
 selectTheme(htmlFolder)
 
 print("DONE!")
