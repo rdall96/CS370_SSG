3aeebe725253e692691aeccf9e1755acad33f622
Ricky Dall'Armellina <rdall96@gmail.com>
2018-10-26 11:50:13 -0400

    Option to delete already existing website
    Program checks is selected path to save the website already exists. If it does it asks the user to delete it and proceed. If the user does not want to delete it, the program will prompt for a new path and will repeat until that path is okay to write to.

--- Flock/main.py
+++ Flock/main.py
@@ -15,7 +15,7 @@ def LOG(string):
     return
 
 # IMPORTS
-import os, sys
+import os, shutil, sys
 import src.converter.markdown2html as Converter
 import src.docs.showDocs as UsageDocs
 import src.stubber.Stubber2 as Stubber
@@ -29,6 +29,27 @@ import src.themeSelector as Themer
 
 def getFullPath(folder):
     return os.path.abspath(folder + '/')
+
+def checkExistingDestination(destinationFolder):
+    # checking if user selected destination path is valid to save a website
+    NEED_NEW_FOLDER = False # tells the program if it needs to request a new destination path
+    
+    LOG("Checking if selected destination path already exists")
+    pathExists = os.path.isdir(destinationFolder)
+    if pathExists:
+        print("Selected destination '" + destinationFolder + "' already contains a website folder.")
+        checkDelete = input("Would you like to delete it? [yes/no] ")
+        if checkDelete == 'y' or checkDelete == 'Y' or checkDelete == 'yes' or checkDelete == 'Yes' or checkDelete == 'YES':
+            LOG("Removing existing destination directory")
+            shutil.rmtree(destinationFolder, ignore_errors=True)
+            print("Folder removed")
+        else:
+            LOG("Decided not to remove destination folder")
+            print("Cannot proceed in this destination path")
+            NEED_NEW_FOLDER = True # set this to tru to request new path upon return
+    else:
+        LOG("Selected destination path is valid")
+    return NEED_NEW_FOLDER
 
 def renameIndex(folder):
     # renames index website file - file name needs to contain index as stub
@@ -54,6 +75,9 @@ markdownFolder = OSutil.systemCheck(input("Insert path to markdown documents: ")
 LOG("Source folder:  " + markdownFolder)
 # Adding '/www' to the destination path to make sure it's an non-existing path to save the website
 htmlFolder = OSutil.systemCheck(input("Insert path to the website folder: ")) + "/www"
+# keep requesting new destination folder until path is okay to use
+while checkExistingDestination(htmlFolder):
+    htmlFolder = OSutil.systemCheck(input("Insert path to the website folder: ")) + "/www"
 LOG("Destination folder:  " + htmlFolder)
 
 # Create htmlFolder directory structure and copy files to it
